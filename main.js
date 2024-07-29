9f6259352ce1f57da0f14ab45fd70a75fa5588fc
Mauricio <mauriciosm95@gmail.com>
2024-07-29 09:14:53 -0300

    feat: implementa upload dos arquivos no ftp e no mongo

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-const { app, Tray, Menu, nativeImage, BrowserWindow, dialog, shell } = require('electron');
-const path = require('path');
-const fileManager = require('./lib/fileManager');
-const database = require('./lib/database');
+const {
+  app,
+  Tray,
+  Menu,
+  nativeImage,
+  BrowserWindow,
+  dialog,
+  shell,
+} = require("electron");
+require("dotenv").config();
+const path = require("path");
+const fs = require("fs");
+
+const fileManager = require("./lib/fileManager");
+const database = require("./lib/database");
 
 let tray;
 let browserWindow;
-let selectedPath = '';
 
 app.whenReady().then(() => {
-  let icon = nativeImage.createFromPath(path.join(__dirname, 'src/public/images/favicon.ico'));
+  let icon = nativeImage.createFromPath(
+    path.join(__dirname, "src/public/images/favicon.ico")
+  );
   icon = icon.resize({ height: 16, width: 16 });
   tray = new Tray(icon);
 
@@ -16,70 +28,102 @@ app.whenReady().then(() => {
     {
       label: "Selecionar Repositório com Arquivos",
       click: async () => {
-        const result = await dialog.showOpenDialog({ properties: ["openDirectory"] });
+        const result = await dialog.showOpenDialog({
+          properties: ["openDirectory"],
+        });
         if (!result.canceled) {
-          selectedPath = result.filePaths[0];
-          console.log("Selected path:", selectedPath);
-          const filesBase64 = await fileManager.getFilesInDirectoryAsBase64(selectedPath);
-          await database.saveFilesToDatabase(filesBase64);
+          const selectedPath = result.filePaths[0];
+          fs.readdir(selectedPath, async (err, files) => {
+            if (err) {
+              console.error("Erro ao ler a pasta:", err);
+              return;
+            }
+            for (const file of files) {
+              const localPath = path.join(selectedPath, file);
+              const remotePath = `uploads/${file}`;
+              try {
+                const ftpUrl = await fileManager.uploadToFTP(
+                  localPath,
+                  remotePath
+                );
+                await database.saveFileToDatabase(localPath, ftpUrl);
+                console.log(
+                  `Arquivo ${file} enviado para o FTP e sua Ref salva no MongoDB.`
+                );
+              } catch (error) {
+                console.error(
+                  "Erro ao enviar o arquivo para o FTP ou salvar no MongoDB:",
+                  error
+                );
+              }
+            }
+          });
         }
       },
     },
     {
-      label: 'Simular Popup', type: 'normal', click: () => {
-        dialog.showMessageBox({ title: 'Aviso', message: 'Você clicou no botão de abrir janela' });
-      }
+      label: "Simular Popup",
+      type: "normal",
+      click: () => {
+        dialog.showMessageBox({
+          title: "Aviso",
+          message: "Você clicou no botão de abrir janela",
+        });
+      },
     },
     {
-      label: 'Histórico de Transferências', type: 'normal', click: () => {
+      label: "Histórico de Transferências",
+      type: "normal",
+      click: () => {
         Menu.setApplicationMenu(null);
         browserWindow = new BrowserWindow({
-          icon: path.join(__dirname, 'src/public/images/favicon.ico'),
-          vibrancy: 'dark',
+          icon: path.join(__dirname, "src/public/images/favicon.ico"),
+          vibrancy: "dark",
           darkTheme: true,
           autoHideMenuBar: true,
-          title: 'Histórico de Transferências',
+          title: "Histórico de Transferências",
           width: 1200,
           height: 800,
           webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false
-          }
+            contextIsolation: false,
+          },
         });
-        browserWindow.loadFile(path.join(__dirname, 'src/views/index.html'));
-      }
+        browserWindow.loadFile(path.join(__dirname, "src/views/index.html"));
+      },
     },
-    { type: 'separator' },
+    { type: "separator" },
     {
-      label: 'Suporte', type: 'normal',
+      label: "Suporte",
+      type: "normal",
       click: () => {
-        shell.openExternal('https://google.com/');
-      }
+        shell.openExternal("https://google.com/");
+      },
     },
-    { type: 'separator' },
+    { type: "separator" },
     { role: "quit", label: "Fechar" },
   ]);
 
   tray.setContextMenu(contextMenu);
-  tray.setToolTip('Upload do CBPM');
-  tray.setTitle('BiometricRouter');
+  tray.setToolTip("Upload do CBPM");
+  tray.setTitle("BiometricRouter");
 
   tray.on("click", (event, bounds) => {
     tray.popUpContextMenu(contextMenu, bounds);
   });
 
-  tray.on('right-click', () => {
+  tray.on("right-click", () => {
     tray.popUpContextMenu(contextMenu);
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
